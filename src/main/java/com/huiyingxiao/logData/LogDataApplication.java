982fef45cfd393798fec26fe75de6ba1179b5530
lyf451296108 <451296108@qq.com>
2018-06-21 10:16:15 +0800

    init project

---
+++ src/main/java/com/huiyingxiao/logData/LogDataApplication.java
@@ -0,0 +1,12 @@
+package com.huiyingxiao.logData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogDataApplication.class, args);
+	}
+}
